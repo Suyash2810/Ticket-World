3963a2d690365c1c1c357a734c8bdf104d349a58
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2018-10-20 18:45:50 +0530

    Created the classes and their objects.

---
+++ scripts/App.js
@@ -0,0 +1,5 @@
+//Instantiation of both the classes
+
+const eventBrite = new event_Brite();
+const ui = new UserInterface();
+console.log(eventBrite);
---
+++ scripts/Event_Brite.js
@@ -0,0 +1,6 @@
+class event_Brite{
+    
+    constructor(){
+        this.token_authentication = 'MSNWHMORIZCPIYNABE7K';
+    }
+}
