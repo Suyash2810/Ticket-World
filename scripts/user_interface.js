3e779d1d30988c4b8e9d1710be65de29f62eff5c
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2018-10-20 20:13:41 +0530

    Added the options list

--- scripts/user_interface.js
+++ scripts/user_interface.js
@@ -10,7 +10,19 @@ class UserInterface {
     selectCategories() {
         const categoriesList = eventbrite.getCategoriesAPI()
         .then(function(data){
-            console.log(data);
+            const Data =  data.categoriesData.categories;
+            const categoriesSelect = document.querySelector('#category');
+            
+            Data.forEach(function(Category){
+                const option = document.createElement('option');
+                option.value = Category.id;
+                option.appendChild(document.createTextNode(Category.name));
+                categoriesSelect.appendChild(option);
+            })
+        })
+        .catch(function(error)
+              {
+            console.log("Error");
         });
     }
 }
