bf4f51575ec480080622d00c44a7c13e7c9f81e5
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2018-10-21 13:32:04 +0530

    Added the asynchronous function for querying the api

--- scripts/App.js
+++ scripts/App.js
@@ -4,18 +4,15 @@ const ui = new UserInterface();
 
 //Event listeners and variables
 
-document.getElementById('submitBtn').addEventListener('click',(e)=>{
-    
+document.getElementById('submitBtn').addEventListener('click', (e) => {
+
     const eventName = document.getElementById('name_event').value;
     const category_event = document.getElementById('category').value;
-    
-    
-    if(eventName !== '')
-        {
-            
-        }
-    else
-        {
-            ui.printErrorMessage('Add an event name or the name of the city.','alert alert-danger mt-4');
-        }
+
+
+    if (eventName !== '') {
+        eventbrite.queryingAPI(eventName,category_event);
+    } else {
+        ui.printErrorMessage('Add an event name or the name of the city.', 'alert alert-danger mt-4');
+    }
 });
--- scripts/EventBrite.js
+++ scripts/EventBrite.js
@@ -1,23 +1,27 @@
 class eventBrite {
-    
-    constructor(){
+
+    constructor() {
         this.token_authentication = 'MSNWHMORIZCPIYNABE7K';
+        this.sortBy = 'date';
     }
-    
-    
+
+
     //getting the categories from the API
-    
-   async getCategoriesAPI(){
+
+    async queryingAPI(name, category1) {
+        const EventResponse = await fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${name}&sort_by=${this.sortBy}&categories=${category1}&token=${this.token_authentication}`);
+    }
+
+
+    async getCategoriesAPI() {
         const categoriesResponse = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.token_authentication}`);
-        
+
         //Converting the categoriesResponse as JSON
-        
+
         const categoriesData = await categoriesResponse.json();
-        
+
         return {
             categoriesData
         }
     }
-    
-    
 }
