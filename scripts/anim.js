8a832f8710636dca3e41123208319b9e32652b54
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2018-10-27 01:39:24 +0530

    Added animation to the buttons

--- scripts/anim.js
+++ scripts/anim.js
@@ -1,32 +1,36 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     var obj = anime({
         targets: '#head_top_left',
-        translateX: ['-250px','0px'],
-        duration: function(el,i,l){
-            return 2000 + (100*i);
+        translateX: ['-250px', '0px'],
+        duration: function (el, i, l) {
+            return 2000 + (100 * i);
         },
-        delay: function(el,i,l){
-            return 2000*i+2500;
+        delay: function (el, i, l) {
+            return 2000 * i + 2500;
         },
-        elasticity: function(el,i,l){
-            return 500 + (200*i);
+        elasticity: function (el, i, l) {
+            return 500 + (200 * i);
         },
-        scale:[0.5,1.0]
+        scale: [0.5, 1.0]
     });
-//    
-//    var timelineHead = anime.timeline();
-//    
-//    timelineHead
-//    .add({
-//        targets: '#timeline1',
-//        translateX:250,
-//        easing:'easeOutExpo'
-//    })
-//    .add({
-//        targets:'#timeline2',
-//        translateX:250,
-//        easing:'easeOutExpo'
-//    })
-    
+
+    var timelineHead = anime.timeline();
+
+    timelineHead
+        .add({
+            targets: '#submitBtn',
+            translateX: ['-850px', '0px'],
+            easing: 'easeOutExpo',
+            duration: 4000,
+            delay:500
+        })
+        .add({
+            targets: '#clearBtn',
+            translateX: ['850px', '0px'],
+            easing: 'easeOutExpo',
+            duration: 4800,
+        offset:4000
+        })
+
 });
